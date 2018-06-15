892d7611521c0c1f3a2cf9eae32dfe71da1106ea
piter <pdpiatek@gmail.com>
2018-06-15 11:20:05 +0200

    simple contact page

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', include('personal.urls')),
+    url(r'^', include('personal.urls')),
 ]
